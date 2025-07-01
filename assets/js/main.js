33f8a8728aa26714cb08eced64b7a3cfccdae8ad
llreebor <timoschuk.roman@gmail.com>
2025-07-01 16:00:38 +0300

    add new filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -367,6 +367,99 @@ function toggleFiltersVisibility() {
 // Initialize the filters functionality
 toggleFiltersVisibility()
 
+// Slide Animation
+/* SLIDE UP */
+let slideUp = (target, duration = 300) => {
+	target.style.transitionProperty = 'height, margin, padding'
+	target.style.transitionDuration = duration + 'ms'
+	target.style.boxSizing = 'border-box'
+	target.style.height = target.offsetHeight + 'px'
+	target.offsetHeight
+	target.style.overflow = 'hidden'
+	target.style.height = 0
+	target.style.paddingTop = 0
+	target.style.paddingBottom = 0
+	target.style.marginTop = 0
+	target.style.marginBottom = 0
+	target.style.border = 'none'
+
+	window.setTimeout(() => {
+		target.style.display = 'none'
+		target.style.removeProperty('height')
+		target.style.removeProperty('padding-top')
+		target.style.removeProperty('padding-bottom')
+		target.style.removeProperty('margin-top')
+		target.style.removeProperty('margin-bottom')
+		target.style.removeProperty('overflow')
+		target.style.removeProperty('transition-duration')
+		target.style.removeProperty('transition-property')
+		target.style.removeProperty('border')
+	}, duration)
+}
+/* SLIDE DOWN */
+let slideDown = (target, duration = 300) => {
+	target.style.removeProperty('display')
+	let display = window.getComputedStyle(target).display
+	if (display === 'none') display = 'grid'
+	target.style.display = display
+	let height = target.offsetHeight
+	target.style.overflow = 'hidden'
+	target.style.height = 0
+	target.style.paddingTop = 0
+	target.style.paddingBottom = 0
+	target.style.marginTop = 0
+	target.style.marginBottom = 0
+	target.offsetHeight
+	target.style.boxSizing = 'border-box'
+	target.style.transitionProperty = 'height, margin, padding'
+	target.style.transitionDuration = duration + 'ms'
+	target.style.height = height + 'px'
+	target.style.border = 'none'
+
+	target.style.removeProperty('padding-top')
+	target.style.removeProperty('padding-bottom')
+	target.style.removeProperty('margin-top')
+	target.style.removeProperty('margin-bottom')
+	target.style.removeProperty('border')
+
+	window.setTimeout(() => {
+		target.style.removeProperty('height')
+		target.style.removeProperty('overflow')
+		target.style.removeProperty('transition-duration')
+		target.style.removeProperty('transition-property')
+		target.style.removeProperty('border')
+	}, duration)
+}
+/* TOOGLE */
+const slideToggle = (target, duration = 300) => {
+	if (window.getComputedStyle(target).display === 'none') {
+		return slideDown(target, duration)
+	} else {
+		return slideUp(target, duration)
+	}
+}
+
+/**
+ * Initializes the toggle functionality for showing/hiding filters on mobile.
+ * When the "More Filters" button is clicked, the hidden filters section
+ * will slide open or closed, and the button will toggle its active state.
+ */
+function initMobileFilterToggle() {
+	const hiddenFilters = document.querySelector('.filters__group')
+	const triggerBtn = document.querySelector('.filters__more-btn')
+
+	// Exit early if required elements are not found
+	if (!hiddenFilters || !triggerBtn) return
+
+	triggerBtn.addEventListener('click', () => {
+		slideToggle(hiddenFilters)
+		triggerBtn.classList.toggle('active')
+	})
+}
+
+// Initialize the filter toggle functionality
+initMobileFilterToggle()
+
 // Autocomplete
 function initializeAutocomplete(selector, suggestions) {
 	const inputs = document.querySelectorAll(selector)
@@ -432,4 +525,4 @@ const rechtsgebieden = [
 	'Burgerlijk recht',
 ]
 
-initializeAutocomplete('.autocomplete-input', rechtsgebieden)
+initializeAutocomplete('.autocomplete-input-area', rechtsgebieden)
